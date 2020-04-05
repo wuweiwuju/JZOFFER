6055dbcaef23230f2cee99e98130dc0fad0f92c9
李鹏飞 <305086028@qq.com>
2020-04-05 22:35:09 +0800

    上传代码

---
+++ mian37.序列化二叉树/mian37.序列化二叉树/test.cpp
@@ -0,0 +1,48 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Codec {
+public:
+
+	// Encodes a tree to a single string.
+	string serialize(TreeNode* root) {
+		if (root == NULL)
+			return "#_";
+		string res = to_string(root->val) + "_";
+		res += serialize(root->left);
+		res += serialize(root->right);
+		return res;
+	}
+
+	// Decodes your encoded data to tree.
+	TreeNode* deserialize(string data) {
+		std::stringstream ss(data);
+		std::string item;
+		queue<string> q;
+		while (std::getline(ss, item, '_'))
+			q.push(item);
+		return helper(q);
+	}
+	TreeNode* helper(queue<string>& q)
+	{
+		string val = q.front();
+		q.pop();
+		if (val == "#")
+			return NULL;
+		TreeNode* head = new TreeNode(stoi(val));
+		head->left = helper(q);
+		head->right = helper(q);
+		return head;
+	}
+};
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec;
+// codec.deserialize(codec.serialize(root));
