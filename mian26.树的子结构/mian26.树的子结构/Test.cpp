8ce690002f700ec50ac3629bf607763f8b9a8754
李鹏飞 <305086028@qq.com>
2020-04-11 21:20:56 +0800

    上传代码

---
+++ mian26.树的子结构/mian26.树的子结构/Test.cpp
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*
+struct TreeNode {
+	int val;
+	struct TreeNode *left;
+	struct TreeNode *right;
+	TreeNode(int x) :
+			val(x), left(NULL), right(NULL) {
+	}
+};*/
+class Solution {
+public:
+	bool DoesTree1HaveTree2(TreeNode* pRoot1, TreeNode* pRoot2)
+	{
+		if (pRoot2 == nullptr)
+			return true;
+		if (pRoot1 == nullptr)
+			return false;
+		if (pRoot1->val != pRoot2->val)
+			return false;
+		return DoesTree1HaveTree2(pRoot1->left, pRoot2->left) &&
+			DoesTree1HaveTree2(pRoot1->right, pRoot2->right);
+	}
+	bool HasSubtree(TreeNode* pRoot1, TreeNode* pRoot2)
+	{
+		bool result = false;
+
+		if (pRoot1 != nullptr && pRoot2 != nullptr)
+		{
+			if (pRoot1->val == pRoot2->val)
+				result = DoesTree1HaveTree2(pRoot1, pRoot2);
+			if (!result)
+				result = HasSubtree(pRoot1->left, pRoot2);
+			if (!result)
+				result = HasSubtree(pRoot1->right, pRoot2);
+		}
+
+		return result;
+	}
+};
