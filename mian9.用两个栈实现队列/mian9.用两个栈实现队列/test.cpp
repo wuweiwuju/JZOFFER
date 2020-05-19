e72803617b69e7dc8657bc57f9c634031c778ad0
李鹏飞 <305086028@qq.com>
2020-05-19 22:11:39 +0800

    上传代码

--- mian9.用两个栈实现队列/mian9.用两个栈实现队列/test.cpp
+++ mian9.用两个栈实现队列/mian9.用两个栈实现队列/test.cpp
@@ -1,3 +1,8 @@
+/*#include<iostream>
+#include<string>
+#include<stack>
+#include<vector>
+using namespace std;
 class Solution
 {
 public:
@@ -24,3 +29,57 @@ private:
 	stack<int> stack1;
 	stack<int> stack2;
 };
+int main()
+{
+	int N;
+	cin >> N;
+	vector<int> res;
+
+}*/
+
+#include<iostream>
+#include<vector>
+#include<string>
+#include<queue>
+using namespace std;
+int main()
+{
+	int n;
+	vector<int> res;
+	queue<int> arr;
+	while (cin >> n) {
+		vector<string> str(n);
+		getline(cin, str[0]);
+		for (auto& e : str)
+			getline(cin, e);
+
+		for (int i = 0; i < n; ++i) {
+			if (str[i] == "peek") {
+				if (!arr.empty())
+					res.push_back(arr.front());
+			}
+			else if (str[i] == "pool") {
+				if (!arr.empty())
+					arr.pop();
+			}
+			else {
+				string temp = str[i];
+				string tmp;
+				int count = 0;
+				for (int i = 0; i < temp.size(); ++i) {
+					if (temp[i] >= '0' && temp[i] <= '9') {
+						tmp += temp[i];
+					}
+				}
+				arr.push(atoi(tmp.c_str()));
+				tmp.clear();
+			}
+		}
+		str.clear();
+
+		for (auto& e : res)    cout << e << endl;
+		res.clear();
+	}
+
+	return 0;
+}
